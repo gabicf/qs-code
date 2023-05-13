8fbc39b0d95265f38baa6d0c7aecb16585e1cd98
gabicf <98752336+gabicf@users.noreply.github.com>
2023-05-13 19:30:16 +0000

    TDD_RED_EX02

---
+++ QUIZ_EX2/power_test.py
@@ -0,0 +1,23 @@
+import unittest
+
+class PowerTest:
+    def powerTest1(self):
+        """Testando a primeira equação de 2 e 3 """
+        self.assertEqual(PowerSample.power(2,3)8)
+
+    def powerTest2(self):
+        """Testando a primeira equação de 2 e 3 """
+        self.assertEqual(PowerSample.power(2,3)8)
+
+    def powerTest3(self):
+        """Testando a primeira equação de 2 e 3 """
+        self.assertEqual(PowerSample.power(2,3)8)
+
+    def powerTest4(self):
+        """Testando a primeira equação de 2 e 3 """
+        self.assertEqual(PowerSample.power(2,3)8)
+
+    def powerTest5(self):
+        """Testando a primeira equação de 2 e 3 """
+        self.assertEqual(PowerSample.power(2,3)8)
+    
