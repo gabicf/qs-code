dbb0800cca52b84fe172a8ee1a732425b845baf3
gabicf <98752336+gabicf@users.noreply.github.com>
2023-05-15 14:14:20 +0000

    TDD_purble_EX03

--- aula_01/QUIZ_EX3/factorial_test.py
+++ aula_01/QUIZ_EX3/factorial_test.py
@@ -4,4 +4,9 @@ from factorial_sample import Factorial_Sample
 class Test_Factorial(unittest.TestCase):
     def test_factorial(self):
         self.assertEqual(Factorial_Sample.factorial(5),120)
-
+        self.assertEqual(Factorial_Sample.factorial(0), 1)
+        self.assertEqual(Factorial_Sample.factorial(1), 1)
+        self.assertEqual(Factorial_Sample.factorial(2), 2)
+        self.assertEqual(Factorial_Sample.factorial(10), 3628800)
+        self.assertEqual(Factorial_Sample.factorial(6), 720)
+        self.assertEqual(Factorial_Sample.factorial(3), 6)
