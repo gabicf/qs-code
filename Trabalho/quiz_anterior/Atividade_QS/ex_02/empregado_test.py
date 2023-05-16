c483be401912fd0b62fb0c5da5349aadbe89f80a
gabicf <98752336+gabicf@users.noreply.github.com>
2023-05-16 02:11:52 +0000

    TDD_PURPLE_TRAB_EX_02

--- Trabalho/quiz_anterior/Atividade_QS/ex_02/empregado_test.py
+++ Trabalho/quiz_anterior/Atividade_QS/ex_02/empregado_test.py
@@ -3,16 +3,17 @@ from empregado_sample import Empregado
 
 class Teste_Empregado(unittest.TestCase):
     def test_calcular_reajuste(self):
-        empregado = Empregado('Caique', 'Antunes', 'Programador', 2000, 1.05)
+        empregado = Empregado('Caique', 'Antunes', 'presidente', 2000, 1.05)
         self.assertEqual(empregado.calcular_reajuste(empregado.retornasalario()), 2100)
 
     def test_fullname(self):
-        empregado = Empregado('Caique', 'Antunes', 'Programador', 2000, 1.05)
+        empregado = Empregado('Caique', 'Antunes', 'presidente', 2000, 1.05)
         self.assertEqual(empregado.gerar_nome_completo(empregado.retornaPnome(), empregado.retornasobrenome()), 'Caique Antunes')
 
     def test_valida_cargo(self):
-        empregado = Empregado('Caique', 'Antunes', 'Programador', 2000, 1.05)
+        empregado = Empregado('Caique', 'Antunes', 'presidente', 2000, 1.05)
         self.assertEqual(empregado.validar_cargo(empregado.retornacargo()), 'Cargo Valido')
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_valida_cargo(self):
+        empregado = Empregado('Caique', 'Antunes', 'Programador', 2000, 1.05)
+        self.assertEqual(empregado.validar_cargo(empregado.retornacargo()), 'Cargo não válido')
