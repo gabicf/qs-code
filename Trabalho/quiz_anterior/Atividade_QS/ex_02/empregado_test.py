74125da20d518d98222cd42728536319f361a862
gabicf <98752336+gabicf@users.noreply.github.com>
2023-05-16 02:10:30 +0000

    TDD_GREEN_TRAB_EX_02_correcao

--- Trabalho/quiz_anterior/Atividade_QS/ex_02/empregado_test.py
+++ Trabalho/quiz_anterior/Atividade_QS/ex_02/empregado_test.py
@@ -1,35 +1,18 @@
-class Empregado:
-    allCargos = ['presidente', 'diretor', 'gerente', 'analista', 'auxiliar']
+import unittest
+from empregado_sample import Empregado
 
-    def __init__(self, primeiro_nome, sobrenome, cargo, salario, taxa_reajuste):
-        self.primeiro_nome = primeiro_nome
-        self.sobrenome = sobrenome
-        self.cargo = cargo
-        self.salario = salario
-        self.taxa_reajuste = taxa_reajuste
+class Teste_Empregado(unittest.TestCase):
+    def test_calcular_reajuste(self):
+        empregado = Empregado('Caique', 'Antunes', 'Programador', 2000, 1.05)
+        self.assertEqual(empregado.calcular_reajuste(empregado.retornasalario()), 2100)
 
-    def retornaPnome(self):
-        return self.primeiro_nome
-    
-    def retornasobrenome(self):
-        return self.sobrenome
+    def test_fullname(self):
+        empregado = Empregado('Caique', 'Antunes', 'Programador', 2000, 1.05)
+        self.assertEqual(empregado.gerar_nome_completo(empregado.retornaPnome(), empregado.retornasobrenome()), 'Caique Antunes')
 
-    def retornacargo(self):
-        return self.cargo
+    def test_valida_cargo(self):
+        empregado = Empregado('Caique', 'Antunes', 'Programador', 2000, 1.05)
+        self.assertEqual(empregado.validar_cargo(empregado.retornacargo()), 'Cargo Valido')
 
-    def retornasalario(self):
-        return self.salario
-
-    def retornataxa_reajuste(self):
-        return self.taxa_reajuste
-
-    def calcular_reajuste(self, salario):
-        return salario * self.taxa_reajuste
-
-    def gerar_nome_completo(self, nome, sobrenome):
-        return nome + " " + sobrenome
-
-    def validar_cargo(self, cargo):
-        if cargo in self.allCargos:
-            return 'Cargo Valido'
-        return 'Cargo não válido'
+if __name__ == '__main__':
+    unittest.main()
